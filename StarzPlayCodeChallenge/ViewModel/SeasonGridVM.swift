114435de5eb85c09b64e5199ceed3783e4edefe1
TanveerMudassar <mudassarahmad09@gmail.com>
2022-09-09 12:40:49 +0500

    Added all viewModel

---
+++ StarzPlayCodeChallenge/ViewModel/SeasonGridVM.swift
@@ -0,0 +1,31 @@
+//
+//  SessionGridVM.swift
+//  StarzPlayCodeChallenge
+//
+//  Created by Qazi Ammar Arshad on 09/09/2022.
+//
+
+import Foundation
+
+extension SeasonGridView{
+    final class SeasonGridVM:ObservableObject {
+
+        @Published var seasons = [Season]()
+
+        init(){
+            seasons = [Season(name: "SEASON 1", isSelecte: true), Season(name: "SEASON 2", isSelecte: false), Season(name: "SEASON 3", isSelecte: false), Season(name: "SEASON 4", isSelecte: false)]
+        }
+
+    }
+}
+extension SeasonGridView.SeasonGridVM {
+    func update(selecteItem: Season){
+        for (index , _) in seasons.enumerated() {
+            if seasons[index].id == selecteItem.id {
+                seasons[index].isSelecte = true
+            }else {
+                seasons[index].isSelecte = false
+            }
+        }
+    }
+}
