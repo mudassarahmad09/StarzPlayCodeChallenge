1b918e39b73babe8470d9605647294b1f3434e6f
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2024-08-28 01:08:44 +0500

    fix some aligment issue here

--- StarzPlayCodeChallenge/MainAppFactory/AppInstantiationFactory.swift
+++ StarzPlayCodeChallenge/MainAppFactory/AppInstantiationFactory.swift
@@ -8,65 +8,65 @@
 import Foundation
 import SwiftUI
 
- struct AppInstantiationFactory {
+struct AppInstantiationFactory {
      
      func startApp() -> HomeView {
-        homeView()
-    }
-    
+          homeView()
+     }
+     
      func homeView() -> HomeView {
           HomeView(
                viewModel: HomeViewModel(mediaEndpointConfig: MediaEndPointManger.createEndpoints())
           )
      }
-    
-    func detailView(for mediaId: Int, and contentType: ContentType) -> AnyView {
-        if contentType == .person {
-          return AnyView (personDetailView(for: mediaId))
-        }
-        return AnyView(mediaDetailView(for: mediaId, contentType: contentType))
-    }
-    
-    private func mediaDetailView(for mediaId: Int, contentType: ContentType) -> DetailView {
-       let vm = makeDetailVM(for: mediaId, and: contentType)
-       return DetailView(
-           viewModel: vm,
-           dynamicContentView: {
-               self.makeDetailSubView(for: mediaId, contentType: contentType, viewModel: vm)
-           },
-           url: videoUrl()
-       )
-   }
-    
-    func personDetailView(for mediaId: Int) -> PersonDetailView {
-        let vm = PersonDetailVM(detailService: PersonDetailServiceAdapter(), id: mediaId)
-        return PersonDetailView(viewModel: vm)
-    }
-    
-    private func makeDetailVM(for mediaId: Int, and contentType: ContentType) -> DetailVM {
-         DetailVM(detailService: contentType.adpter, id: mediaId)
-    }
-    
-    private func makeDetailSubView(for mediaId: Int,contentType: ContentType, viewModel: DetailVM) -> AnyView? {
-        contentType == .movie ? recommendationView(viewModel) : seasonView(viewModel, mediaId)
-    }
-    
-      private func recommendationView(_ viewModel: DetailVM) -> AnyView? {
-           guard let movieDetail = viewModel.detail as? MovieDetail else { return nil }
-           guard let recommendations = movieDetail.getRecommendations()?.results ,
-                 !recommendations.isEmpty else { return nil }
-           return AnyView(PosterRowView(layout: Layout( sectionTitle: "Recommendations", titles: recommendations)))
-      }
-    
-      private func seasonView(_ viewModel: DetailVM, _ mediaId: Int) -> AnyView? {
-           guard let tvDetail = viewModel.detail as? TvDetail else { return nil }
-           guard let seasons = tvDetail.getSeasons(),
-                 !seasons.isEmpty else { return nil }
-           let seasonVM =  SeasonGridVM(seaasonService: EpisodeServiceAdpter(),seasons: seasons)
-           return AnyView(SeasonGridView(viewModel: seasonVM, mediaId: mediaId))
-      }
-    
-    private func videoUrl() -> URL {
-        URL(string: AppUrl.VURL)!
-    }
+     
+     func detailView(for mediaId: Int, and contentType: ContentType) -> AnyView {
+          if contentType == .person {
+               return AnyView (personDetailView(for: mediaId))
+          }
+          return AnyView(mediaDetailView(for: mediaId, contentType: contentType))
+     }
+     
+     private func mediaDetailView(for mediaId: Int, contentType: ContentType) -> DetailView {
+          let vm = makeDetailVM(for: mediaId, and: contentType)
+          return DetailView(
+               viewModel: vm,
+               dynamicContentView: {
+                    self.makeDetailSubView(for: mediaId, contentType: contentType, viewModel: vm)
+               },
+               url: videoUrl()
+          )
+     }
+     
+     func personDetailView(for mediaId: Int) -> PersonDetailView {
+          let vm = PersonDetailVM(detailService: PersonDetailServiceAdapter(), id: mediaId)
+          return PersonDetailView(viewModel: vm)
+     }
+     
+     private func makeDetailVM(for mediaId: Int, and contentType: ContentType) -> DetailVM {
+          DetailVM(detailService: contentType.adpter, id: mediaId)
+     }
+     
+     private func makeDetailSubView(for mediaId: Int,contentType: ContentType, viewModel: DetailVM) -> AnyView? {
+          contentType == .movie ? recommendationView(viewModel) : seasonView(viewModel, mediaId)
+     }
+     
+     private func recommendationView(_ viewModel: DetailVM) -> AnyView? {
+          guard let movieDetail = viewModel.detail as? MovieDetail else { return nil }
+          guard let recommendations = movieDetail.getRecommendations()?.results ,
+                !recommendations.isEmpty else { return nil }
+          return AnyView(PosterRowView(layout: Layout( sectionTitle: "Recommendations", titles: recommendations)))
+     }
+     
+     private func seasonView(_ viewModel: DetailVM, _ mediaId: Int) -> AnyView? {
+          guard let tvDetail = viewModel.detail as? TvDetail else { return nil }
+          guard let seasons = tvDetail.getSeasons(),
+                !seasons.isEmpty else { return nil }
+          let seasonVM =  SeasonGridVM(seaasonService: EpisodeServiceAdpter(),seasons: seasons)
+          return AnyView(SeasonGridView(viewModel: seasonVM, mediaId: mediaId))
+     }
+     
+     private func videoUrl() -> URL {
+          URL(string: AppUrl.VURL)!
+     }
 }
