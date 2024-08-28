06a1b4ec0fb7072756356206d953e2c263d5470e
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2024-08-28 21:57:51 +0500

    adding the target for tvos now i will add some of the comonet for tvOS as well

---
+++ StarzPlayCodeChallenge/tvOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarzPlayCodeChallengeTV
+//
+//  Created by Qazi Mudassar on 28/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
