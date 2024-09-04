0ed8aea76a0fcd858f9588daed07b17f8ed82592
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2024-09-04 14:06:27 +0500

    change my service into the actor to better deal with race condtion

--- StarzPlayCodeChallenge/iOS/Services/EpisodeService.swift
+++ StarzPlayCodeChallenge/iOS/Services/EpisodeService.swift
@@ -12,7 +12,7 @@ protocol EpisodeService {
     func getEpisodeDetailDetail(tv id: Int, seasonId: Int) async -> Result<Season, RequestError>
 }
 
-struct EpisodeServiceAdpter: NetworkManagerService, EpisodeService {
+actor EpisodeServiceAdpter: NetworkManagerService, EpisodeService {
     func getEpisodeDetailDetail(tv id: Int, seasonId: Int) async -> Result<Season, RequestError> {
         return await sendApiRequest(
             endpoint: SeasonEndpoint.seasonDetail(
--- StarzPlayCodeChallenge/iOS/Services/MediaDetailService.swift
+++ StarzPlayCodeChallenge/iOS/Services/MediaDetailService.swift
@@ -9,21 +9,25 @@ import Foundation
 import MyApiLibrary
 
 protocol MediaDetailService {
-    func getDetails(from id: Int) async -> Result<MediaDetail, RequestError>
+     func getDetails(from id: Int) async -> Result<MediaDetail, RequestError>
 }
 
-struct DetailServiceAdapter<T: Decodable & MediaDetail>: NetworkManagerService, MediaDetailService{
-    let type: ContentType
+actor DetailServiceAdapter<T: Decodable & MediaDetail>: NetworkManagerService, MediaDetailService {
+     let type: ContentType
      
-    func getDetails(from id: Int) async -> Result<MediaDetail, RequestError> {
-        let detailEndPoint = MediaDetailEndPoint.showDetail(id: id, media: type)
-
-        let result = await sendApiRequest(endpoint:detailEndPoint, responseModel: T.self)
-        switch result {
-        case .success(let tvShowsResponse):
-            return .success(tvShowsResponse)
-        case .failure(let error):
-            return .failure(error)
-        }
-    }
+     init(type: ContentType) {
+          self.type = type
+     }
+     
+     func getDetails(from id: Int) async -> Result<MediaDetail, RequestError> {
+          let detailEndPoint = MediaDetailEndPoint.showDetail(id: id, media: type)
+          
+          let result = await sendApiRequest(endpoint:detailEndPoint, responseModel: T.self)
+          switch result {
+          case .success(let tvShowsResponse):
+               return .success(tvShowsResponse)
+          case .failure(let error):
+               return .failure(error)
+          }
+     }
 }
